a2401d1ef8d991555739cba6230458b3f2d7119d
Lauren McBride <lauren@pydojo.com>
2013-07-15 10:45:02 -0400

    updating schema changes

--- resume_app/migrations/0001_initial.py
+++ resume_app/migrations/0001_initial.py
@@ -16,6 +16,7 @@ class Migration(SchemaMigration):
             ('address', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('city', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('state', self.gf('django.db.models.fields.CharField')(max_length=15)),
+            ('zip', self.gf('django.db.models.fields.IntegerField')(default=True, max_length=5)),
             ('telephone', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('email', self.gf('django.db.models.fields.CharField')(max_length=50)),
         ))
@@ -24,9 +25,17 @@ class Migration(SchemaMigration):
         # Adding model 'Skills'
         db.create_table('resume_app_skills', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['resume_app.Category'], null=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(default='', max_length=50)),
         ))
         db.send_create_signal('resume_app', ['Skills'])
+
+        # Adding model 'Category'
+        db.create_table('resume_app_category', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(default='', max_length=50)),
+        ))
+        db.send_create_signal('resume_app', ['Category'])
 
         # Adding model 'Projects'
         db.create_table('resume_app_projects', (
@@ -40,9 +49,11 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('company', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('begin_date', self.gf('django.db.models.fields.DateField')()),
-            ('end_date', self.gf('django.db.models.fields.DateField')()),
+            ('end_date', self.gf('django.db.models.fields.DateField')(default=None, null=True, blank=True)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('description', self.gf('django.db.models.fields.CharField')(max_length=250)),
+            ('description_2', self.gf('django.db.models.fields.CharField')(default=True, max_length=250)),
+            ('description_3', self.gf('django.db.models.fields.CharField')(default=True, max_length=250)),
         ))
         db.send_create_signal('resume_app', ['Experience'])
 
@@ -54,6 +65,9 @@ class Migration(SchemaMigration):
         # Deleting model 'Skills'
         db.delete_table('resume_app_skills')
 
+        # Deleting model 'Category'
+        db.delete_table('resume_app_category')
+
         # Deleting model 'Projects'
         db.delete_table('resume_app_projects')
 
@@ -62,12 +76,19 @@ class Migration(SchemaMigration):
 
 
     models = {
+        'resume_app.category': {
+            'Meta': {'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '50'})
+        },
         'resume_app.experience': {
             'Meta': {'object_name': 'Experience'},
             'begin_date': ('django.db.models.fields.DateField', [], {}),
             'company': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'description': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
-            'end_date': ('django.db.models.fields.DateField', [], {}),
+            'description_2': ('django.db.models.fields.CharField', [], {'default': 'True', 'max_length': '250'}),
+            'description_3': ('django.db.models.fields.CharField', [], {'default': 'True', 'max_length': '250'}),
+            'end_date': ('django.db.models.fields.DateField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
@@ -80,7 +101,8 @@ class Migration(SchemaMigration):
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'lastname': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'state': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
-            'telephone': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+            'telephone': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'zip': ('django.db.models.fields.IntegerField', [], {'default': 'True', 'max_length': '5'})
         },
         'resume_app.projects': {
             'Meta': {'object_name': 'Projects'},
@@ -89,8 +111,9 @@ class Migration(SchemaMigration):
         },
         'resume_app.skills': {
             'Meta': {'object_name': 'Skills'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'to': "orm['resume_app.Category']", 'null': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '50'})
         }
     }
 
